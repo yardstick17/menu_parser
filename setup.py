af81ee85ecdcadb11ca1cb367111263580f226df
Amit Kushwaha <amit_kushwaha@outlook.com>
2016-02-12 13:07:15 +0530

    Added some stuffs

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ from setuptools import setup
 
 setup(
 	name = 'menu_parser' , 
-	vrsion = '0.1' , 
+	vrsion = '0.2' , 
 	description = 'Tool to read images ,images formatted like standard restaurant-menu' , 
 	author = 'Amit Kushwaha' , 
 	author_email = 'amit_kushwaha@outlook.com' , 
 	url = 'https://github.com/yardstick17/menu_parser' ,
-	download_url = 'https://github.com/yardstick17/menu_parser/tarball/0.1'
-	keywords = ['image reader' , 'menu_parser' , 'menu reader']
+	download_url = 'https://github.com/yardstick17/menu_parser/tarball/0.1' , 
+	keywords = ['image reader' , 'menu_parser' , 'menu reader'] , 
 	py_modules = ['menu_parser'] , 
 	include_package_data=True,
     license = 'MIT License',
 	install_requires = [
 			'Click' , 
+			'PIL', 
+			'cv2' , 
+			're' , 
+			'operator'
 	] ,
 	entry_points = '''
 				[console_scripts]
