ea7d840702df9c0b27c59d55f561b937dbda5bf1
Amit Kushwaha <amit_kushwaha@outlook.com>
2016-02-12 12:46:33 +0530

    Updated setup , git file repo url

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
 	name = 'menu_parser' , 
 	vrsion = '0.1' , 
-	description = 'Tool to read images ,images formatted like standard restaurant-menu'
-	author = 'Amit Kushwaha'
-	author_email = 'amit_kushwaha@outlook.com'
-	url = ''
-	download_url = ''
+	description = 'Tool to read images ,images formatted like standard restaurant-menu' , 
+	author = 'Amit Kushwaha' , 
+	author_email = 'amit_kushwaha@outlook.com' , 
+	url = 'https://github.com/yardstick17/menu_parser' ,
+	download_url = 'https://github.com/yardstick17/menu_parser/tarball/0.1'
 	keywords = ['image reader' , 'menu_parser' , 'menu reader']
 	py_modules = ['menu_parser'] , 
 	include_package_data=True,
