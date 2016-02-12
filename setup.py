9800a3b87448c37a5a44d98f46009e3926d76060
Amit Kushwaha <amit_kushwaha@outlook.com>
2016-02-12 10:54:32 +0530

    temp files during processing

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from setuptools import setup
 setup(
 	name = 'menu_parser' , 
 	vrsion = '0.1' , 
+	description = 'Tool to read images ,images formatted like standard restaurant-menu'
+	author = 'Amit Kushwaha'
+	author_email = 'amit_kushwaha@outlook.com'
+	url = ''
+	download_url = ''
+	keywords = ['image reader' , 'menu_parser' , 'menu reader']
 	py_modules = ['menu_parser'] , 
+	include_package_data=True,
+    license = 'MIT License',
 	install_requires = [
 			'Click' , 
 	] ,
